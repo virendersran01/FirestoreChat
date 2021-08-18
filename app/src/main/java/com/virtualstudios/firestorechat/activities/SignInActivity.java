b3fceeeb7cd94d42aacc0140c63a50ed169550f0
virender sran <virendersran01@gmail.com>
2021-08-18 12:27:21 +0530

    Initial commit

---
+++ app/src/main/java/com/virtualstudios/firestorechat/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.virtualstudios.firestorechat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.virtualstudios.firestorechat.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
