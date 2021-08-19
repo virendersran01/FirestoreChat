66243bfe05496a73889058fe76b0331b071e8221
virender sran <virendersran01@gmail.com>
2021-08-19 22:49:07 +0530

    working on signup activity.

--- app/src/main/java/com/virtualstudios/firestorechat/activities/MainActivity.java
+++ app/src/main/java/com/virtualstudios/firestorechat/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.virtualstudios.firestorechat;
+package com.virtualstudios.firestorechat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.virtualstudios.firestorechat.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/virtualstudios/firestorechat/activities/SignUpActivity.java
+++ app/src/main/java/com/virtualstudios/firestorechat/activities/SignUpActivity.java
@@ -3,14 +3,23 @@ package com.virtualstudios.firestorechat.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.virtualstudios.firestorechat.R;
 import com.virtualstudios.firestorechat.databinding.ActivitySignUpBinding;
 
+import java.io.ByteArrayOutputStream;
+
 public class SignUpActivity extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
+    private String encodedImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +30,70 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void setListeners(){
         binding.textSignIn.setOnClickListener(view -> onBackPressed());
+        binding.buttonSignUp.setOnClickListener(view -> {
+            if (isValidSignUpDetails()){
+                signUp();
+            }
+        });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signUp(){
+
+    }
+
+    private String encodedImage(Bitmap bitmap){
+        int previewWidth = 150;
+        int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    //TODO
+    //part 3 == 12:55
+
+    private Boolean isValidSignUpDetails(){
+        if (encodedImage == null){
+            showToast("Select profile image");
+            return false;
+        }else if (binding.inputName.getText().toString().trim().isEmpty()){
+            showToast("Enter name");
+            return false;
+        }else if (binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("Enter valid email");
+            return false;
+        }else if (binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter password");
+            return false;
+        }else if (binding.inputConfirmPassword.getText().toString().trim().isEmpty()){
+            showToast("Confirm your password");
+            return false;
+        }else if (!binding.inputPassword.getText().toString().trim().equals(binding.inputConfirmPassword.getText().toString())){
+            showToast("Password & confirm password must be same");
+            return false;
+        }else {
+            return true;
+        }
+
+    }
+
+    private void loading(Boolean isLoading){
+        if (isLoading){
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+        }
     }
 
 }
