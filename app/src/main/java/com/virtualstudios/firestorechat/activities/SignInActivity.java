0564cab429936846c59250ce99be991af483049c
virender sran <virendersran01@gmail.com>
2021-08-19 21:44:42 +0530

    Done Designing sign in and sign up activities.

--- app/src/main/java/com/virtualstudios/firestorechat/activities/SignInActivity.java
+++ app/src/main/java/com/virtualstudios/firestorechat/activities/SignInActivity.java
@@ -2,15 +2,25 @@ package com.virtualstudios.firestorechat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.virtualstudios.firestorechat.R;
+import com.virtualstudios.firestorechat.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/virtualstudios/firestorechat/activities/SignUpActivity.java
+++ app/src/main/java/com/virtualstudios/firestorechat/activities/SignUpActivity.java
@@ -2,15 +2,25 @@ package com.virtualstudios.firestorechat.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.virtualstudios.firestorechat.R;
+import com.virtualstudios.firestorechat.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
     }
+
+    private void setListeners(){
+        binding.textSignIn.setOnClickListener(view -> onBackPressed());
+    }
+
 }
