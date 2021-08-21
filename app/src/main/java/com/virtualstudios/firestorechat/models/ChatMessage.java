cbc1e05ee67676d27f0f00662ffd21d0c7f148fc
virender sran <virendersran01@gmail.com>
2021-08-21 16:50:12 +0530

    Working on sending receiving messages.

--- app/src/main/java/com/virtualstudios/firestorechat/models/ChatMessage.java
+++ app/src/main/java/com/virtualstudios/firestorechat/models/ChatMessage.java
@@ -1,6 +1,10 @@
 package com.virtualstudios.firestorechat.models;
 
+import java.util.Date;
+
 public class ChatMessage {
 
     public String senderId, receiverId, message, dateTime;
+    public Date dateObject;
+
 }
