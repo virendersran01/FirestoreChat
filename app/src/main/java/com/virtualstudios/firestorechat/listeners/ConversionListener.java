a0efd3e159db3984dd58bfb6d632391e87a3d2d7
virender sran <virendersran01@gmail.com>
2021-08-24 12:51:05 +0530

    Done displaying recent chats on home activity.

---
+++ app/src/main/java/com/virtualstudios/firestorechat/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.virtualstudios.firestorechat.listeners;
+
+import com.virtualstudios.firestorechat.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
--- app/src/main/java/com/virtualstudios/firestorechat/models/ChatMessage.java
+++ app/src/main/java/com/virtualstudios/firestorechat/models/ChatMessage.java
@@ -3,8 +3,7 @@ package com.virtualstudios.firestorechat.models;
 import java.util.Date;
 
 public class ChatMessage {
-
     public String senderId, receiverId, message, dateTime;
     public Date dateObject;
-
+    public String conversionId, conversionName, conversionImage;
 }
